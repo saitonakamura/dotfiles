45a1dca64de58ae823958be1c3759c8739842c91
Mikhail Bashurov <mishinomb@gmail.com>
2019-09-12 04:56:17 -0700

    soft install

---
+++ scripts/install-software.ps1
@@ -0,0 +1,6 @@
+& choco install powershell-core -y `
+  --install-arguments='"ENABLE_PSREMOTING=1 ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1"'
+
+& choco install vscode -y
+
+& choco install neovim -y
