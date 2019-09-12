1014c89ed89bb10f95975dfefbc31cb6281695bb
Saito Nakamura <saitonakamuramb@gmail.com>
2019-09-12 22:12:59 +0900

    assoc file ext

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -19,3 +19,24 @@ function Invoke-AsAdmin([scriptblock] $ScriptBlock) {
 New-Alias -Name "sudo" Invoke-AsAdmin
 
 New-Alias -Name "time" Measure-Command
+
+Function AssociateFileExtensions
+{
+    Param
+    (
+        [Parameter(Mandatory=$true)]
+        [String[]] $FileExtensions,
+        [Parameter(Mandatory=$true)]
+        [String] $OpenAppPath
+    )
+    if (-not (Test-Path $OpenAppPath))
+    {
+	   throw "$OpenAppPath does not exist."
+    }
+    foreach ($extension in $FileExtensions)
+    {
+        $fileType = (cmd /c "assoc $extension")
+        $fileType = $fileType.Split("=")[-1]
+        cmd /c "ftype $fileType=""$OpenAppPath"" ""%1"""
+    }
+}
