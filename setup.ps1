4fad7dde1cff670b59e0583a69109a2e46084a70
Michael Bashurov <accounts@saitonakamura.me>
2020-01-31 00:44:25 +0200

    windows terminal profile

---
+++ setup.ps1
@@ -0,0 +1,3 @@
+New-Item -Item SymbolicLink -Force `
+    -Target "~\dotfiles\WindowsTerminal-profiles.json" `
+    -Path "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\profiles.json"
