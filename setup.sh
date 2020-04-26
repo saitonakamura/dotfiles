7710005c38132db14e88107412532dc592508beb
Michael Bashurov <accounts@saitonakamura.me>
2020-04-26 15:10:19 +0300

    stuff

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ ln -sfn ~/dotfiles/.zshrc ~/.zshrc
 ln -sfn ~/dotfiles/init.vim ~/.config/nvim/init.vim
 ln -sfn ~/dotfiles/vscode/settings.json ~/Library/ApplicationSupport/Code/User/settings.json
 ln -sfn ~/dotfiles/vscode/keybindings.json ~/Library/ApplicationSupport/Code/User/keybindings.json
+ln -sfn ~/dotfiles/vscode/settings.json ~/.vscode-server/data/Machine/settings.json
+ln -sfn ~/dotfiles/vscode/keybindings.json ~/.vscode-server/data/Machine/keybindings.json
 ln -sfn ~/dotfiles/.fdignore ~/.fdignore
 ln -sfn ~/dotfiles/.ideavimrc ~/.ideavimrc
