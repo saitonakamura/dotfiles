f5f17ba1f6e8798c09e6ca64bc23791b775991d1
Michael Bashurov <contact@saitonakamura.me>
2021-01-06 12:20:01 +0200

    vscode and win term

--- scripts/file-assoc.ps1
+++ scripts/file-assoc.ps1
@@ -1,4 +1,7 @@
 $nvimPath = get-command nvim-qt.exe | % Source
+$codePath = get-command code.exe | % Source
 $nvimFiles = @(".log", ".json", ".csproj", ".txt", ".md", ".gitignore")
-AssociateFileExtensions -FileExtensions $nvimFiles -OpenAppPath $nvimPath
+$codeFiles = @(".log", ".json", ".csproj", ".txt", ".md", ".gitignore")
+# AssociateFileExtensions -FileExtensions $nvimFiles -OpenAppPath $nvimPath
+AssociateFileExtensions -FileExtensions $codeFiles -OpenAppPath $codePath
 
