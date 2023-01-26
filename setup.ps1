d5904684069d13be42565545564a56c8d32dd1a0
Michael Bashurov <accounts@saitonakamura.me>
2023-01-26 12:54:31 +0200

    stuff

--- setup.ps1
+++ setup.ps1
@@ -1,17 +1,20 @@
+gsudo {
+    New-Item -Item SymbolicLink -Force `
+        -Target (Resolve-Path "~\dotfiles\WindowsTerminal-profiles.json") `
+        -Path "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+}
 
+gsudo {
+    New-Item -Item SymbolicLink -Force `
+        -Target (Resolve-Path "~\dotfiles\WindowsTerminalPreview-settings.json") `
+        -Path "~\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
+}
 
-
-New-Item -Item SymbolicLink -Force `
-    -Target (Resolve-Path "~\dotfiles\WindowsTerminal-profiles.json") `
-    -Path "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-
-New-Item -Item SymbolicLink -Force `
-    -Target (Resolve-Path "~\dotfiles\WindowsTerminalPreview-settings.json") `
-    -Path "~\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json"
-
-New-Item -Item SymbolicLink -Force `
-    -Target (Resolve-Path "~\dotfiles\winget-settings.json") `
-    -Path "~\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+gsudo {
+    New-Item -Item SymbolicLink -Force `
+        -Target (Resolve-Path "~\dotfiles\winget-settings.json") `
+        -Path "~\AppData\Local\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
+}
 
 mkdir ~\AppData\Local\nvim\autoload
 
@@ -23,13 +26,17 @@ $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   )
 )
 
-New-Item -Item SymbolicLink -Force `
-    -Target (Resolve-Path "~\dotfiles\init.vim") `
-    -Path "~\AppData\Local\nvim\init.vim"
+gsudo {
+    New-Item -Item SymbolicLink -Force `
+        -Target (Resolve-Path "~\dotfiles\init.vim") `
+        -Path "~\AppData\Local\nvim\init.vim"
+}
 
-New-Item -Item SymbolicLink -Force `
-    -Target (Resolve-Path "~\dotfiles\Powershell_profile.ps1") `
-    -Path "$profile"
+gsudo {
+    New-Item -Item SymbolicLink -Force `
+        -Target (Resolve-Path "~\dotfiles\Powershell_profile.ps1") `
+        -Path "$profile"
+}
 
 # Using VSCode Settings Sync now
 # New-Item -Item SymbolicLink -Force `
@@ -40,17 +47,23 @@ New-Item -Item SymbolicLink -Force `
 #     -Target (Resolve-Path "~\dotfiles\vscode\keybindings.json") `
 #     -Path "~\AppData\Roaming\Code\User\keybindings.json"
 
-New-Item -Item SymbolicLink -Force `
-    -Target (Resolve-Path "~\dotfiles\.gitconfig") `
-    -Path "~\.gitconfig"
+gsudo {
+    New-Item -Item SymbolicLink -Force `
+        -Target (Resolve-Path "~\dotfiles\.gitconfig") `
+        -Path "~\.gitconfig"
+}
 
-New-Item -Item SymbolicLink -Force `
-    -Target (Resolve-Path "~\dotfiles\.yarnrc.yml") `
-    -Path "~\.yarnrc.yml"
+gsudo {
+    New-Item -Item SymbolicLink -Force `
+        -Target (Resolve-Path "~\dotfiles\.yarnrc.yml") `
+        -Path "~\.yarnrc.yml"
+}
 
-New-Item -Item SymbolicLink -Force `
-    -Target (Resolve-Path "~\dotfiles\.ideavimrc") `
-    -Path "~\.ideavimrc"
+gsudo {
+    New-Item -Item SymbolicLink -Force `
+        -Target (Resolve-Path "~\dotfiles\.ideavimrc") `
+        -Path "~\.ideavimrc"
+}
 
 Install-Module posh-git -Scope CurrentUser -Force
 # Install-Module oh-my-posh -Scope CurrentUser -Force
