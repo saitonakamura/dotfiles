c8f3890ded0e40cb5dc1c2e05a03e16483cd8c19
Michael Bashurov <accounts@saitonakamura.me>
2023-08-09 20:14:54 +0300

    lua

---
+++ init.lua
@@ -0,0 +1,12 @@
+if vim.g.neovide then
+  vim.o.guifont = 'Cascadia Code PL:h14'
+end
+
+vim.g.mapleader = ' '
+
+
+--if (vim.bo.filetype == 'lua') then
+--  vim.bo.expandtab = true
+--  vim.bo.shiftwidth = 2
+--end
+
