c23d006b1b4de3b60587427deeac7e432166e678
Michael Bashurov <accounts@saitonakamura.me>
2024-07-12 09:15:44 +0300

    vim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,6 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Allow clipboard copy paste with cmd in neovide, otherwise pasting text to terminal or grep is a pain
 if vim.g.neovide then
   vim.keymap.set("v", "<D-c>", '"+y') -- Copy
   vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
@@ -8,9 +10,11 @@ if vim.g.neovide then
   vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
   vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 
-  -- Allow clipboard copy paste in neovim
   vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("t", "<D-v>", "<C-\\><C-n>pi", { noremap = true, silent = true })
   vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
 end
+
+-- save file with cmd+s
+vim.keymap.set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
--- nvim/lua/plugins/tree.lua
+++ nvim/lua/plugins/tree.lua
@@ -1,10 +1,25 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
-    -- opts.filesystem.filtered_items.hide_dotfiles = false
     opts.filesystem.filtered_items = opts.filesystem.filtered_items or {}
     opts.filesystem.filtered_items.hide_dotfiles = false
     opts.filesystem.filtered_items.hide_by_name = { "node_modules", ".git" }
     opts.filesystem.filtered_items.never_show = { ".DS_Store", "thumbs.db", "desktop.ini" }
+
+    opts.window.mappings["Yf"] = opts.window.mappings["Y"]
+    opts.window.mappings["Y"] = nil
+    opts.window.mappings["Yr"] = {
+      function(state)
+        local node = state.tree:get_node()
+        local path = node:get_id()
+        -- local root = require("lazyvim.util").root.get()
+        local relative_path = vim.fn.fnamemodify(path, ":.")
+        -- if vim.startswith(relative_path, root) then
+        --   relative_path = vim.fn.fnamemodify(relative_path, ":s?" .. root .. "/??")
+        -- end
+        vim.fn.setreg("+", relative_path, "c")
+      end,
+      desc = "Copy Root relative path to Clipboard",
+    }
   end,
 }
